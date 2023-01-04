d5b162f6964bcfb3f3f95ad80e002edd2bf2cb01
danandak <danandanurwendaadikusuma_1313621017@mhs.unj.ac.id>
2023-01-04 23:18:31 +0700

    update db

--- db/marketplace_db.sql
+++ db/marketplace_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 31, 2022 at 02:06 AM
+-- Generation Time: Jan 04, 2023 at 05:17 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -31,6 +31,7 @@ CREATE TABLE `cart` (
   `id` int(11) NOT NULL,
   `id_product` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
+  `checked` tinyint(1) DEFAULT 0,
   `total_price` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -54,9 +55,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `description`, `price`, `image`, `stock`) VALUES
-(1, 'Meja Gaming Meja Komputer GG Gemink FLOTH FL120 - Merah', 'READY STOCK !!\r\nProduk Dijamin Baru 100%\r\n=====================\r\nMain game tidak lengkap kalau tidak ada mejanya, jangan lupakan pendukung penting dalam bermain game yaitu meja gaming. Meja Gaming FLOTH FL120 hadir dengan desain sederhana dan harga terjangkau, yang dilengkapi kaki meja metal, dengan edging PVC elegan dengan pilihan warna merah, abu-abu dan biru. Spesifikasi : - Ukuran : 120 x 60 cm - Kaki Metal 5x2.5cm - Edging PVC - High Quality Chipboard - 2x Groumet', 500000, 'assets/image/meja.png', 10),
-(2, 'Baterai Lithium Sony Battery CR2032 CR 2032 CMOS Jam Komputer Laptop', '*Harga persatuan\r\n\r\nBrand: Sony\r\nModel: CR2032\r\nDiameter: 20mm\r\nThickness: 32mm\r\nVoltage: 3V\r\n\r\nCATATAN PENTING\r\n* BARANG BARU\r\n* PRODUK INI TIDAK BERGARANSI\r\n* SERTAKAN VIDEO UNBOXING DARI AWAL TERIMA BARANG SAAT MELAKUKAN KOMPLAIN, TANPA VIDEO UNBOXING KAMI TIDAK AKAN MENERIMA KEKURANGAN / KERUSAKAN\r\n* MEMBELI = SETUJU', 1700, 'assets/image/batre.png', 250),
-(3, 'Mouse Geming Wireless USB Tanpa Kabel Nirkabel Bisa Di Cas Ces Charge', 'SPESIFIKASI\r\nBrand : Divipard\r\nWarna : Hitam\r\nKoneksivitas : Wireless 2.4GHz\r\nCocok digunakan : PC & Laptop\r\nModel : Q3\r\nJarak koneksi : 10m\r\nDPI : 1200-1600-2400-3200\r\n\r\nKEUNGGULAN\r\n- Silent (tidak mengeluarkan suara)\r\n- Mouse charging\r\n- Pengisian cepat\r\n- Memiliki fungsi hemat daya\r\n- 7 warna LED RGB\r\n\r\nDIMENSI\r\nDimensi produk : 14 x 9 x 4 cm\r\nDimensi packaging : 15.5 x 10 x 5.5 cm\r\n\r\nKELENGKAPAN\r\n1 x Mouse Q3\r\n1 x Kabel charge micro', 101000, 'assets/image/mouse.jpg', 48);
+(1, 'Meja Gaming Meja Komputer GG Gemink FLOTH FL120 - Merah', 'READY STOCK !!\r\nProduk Dijamin Baru 100%\r\n=====================\r\nMain game tidak lengkap kalau tidak ada mejanya, jangan lupakan pendukung penting dalam bermain game yaitu meja gaming. Meja Gaming FLOTH FL120 hadir dengan desain sederhana dan harga terjangkau, yang dilengkapi kaki meja metal, dengan edging PVC elegan dengan pilihan warna merah, abu-abu dan biru. Spesifikasi : - Ukuran : 120 x 60 cm - Kaki Metal 5x2.5cm - Edging PVC - High Quality Chipboard - 2x Groumet', 500000, 'assets/image/meja.png', 7),
+(2, 'Baterai Lithium Sony Battery CR2032 CR 2032 CMOS Jam Komputer Laptop', '*Harga persatuan\r\n\r\nBrand: Sony\r\nModel: CR2032\r\nDiameter: 20mm\r\nThickness: 32mm\r\nVoltage: 3V\r\n\r\nCATATAN PENTING\r\n* BARANG BARU\r\n* PRODUK INI TIDAK BERGARANSI\r\n* SERTAKAN VIDEO UNBOXING DARI AWAL TERIMA BARANG SAAT MELAKUKAN KOMPLAIN, TANPA VIDEO UNBOXING KAMI TIDAK AKAN MENERIMA KEKURANGAN / KERUSAKAN\r\n* MEMBELI = SETUJU', 1700, 'assets/image/batre.png', 218),
+(3, 'Mouse Geming Wireless USB Tanpa Kabel Nirkabel Bisa Di Cas Ces Charge', 'SPESIFIKASI\r\nBrand : Divipard\r\nWarna : Hitam\r\nKoneksivitas : Wireless 2.4GHz\r\nCocok digunakan : PC & Laptop\r\nModel : Q3\r\nJarak koneksi : 10m\r\nDPI : 1200-1600-2400-3200\r\n\r\nKEUNGGULAN\r\n- Silent (tidak mengeluarkan suara)\r\n- Mouse charging\r\n- Pengisian cepat\r\n- Memiliki fungsi hemat daya\r\n- 7 warna LED RGB\r\n\r\nDIMENSI\r\nDimensi produk : 14 x 9 x 4 cm\r\nDimensi packaging : 15.5 x 10 x 5.5 cm\r\n\r\nKELENGKAPAN\r\n1 x Mouse Q3\r\n1 x Kabel charge micro', 101000, 'assets/image/mouse.jpg', 32);
 
 --
 -- Indexes for dumped tables
@@ -83,7 +84,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `product`
