640939ff7ad566605343241426bd1f6ed958237e
UnFik <fikriilhamarifin_1313621027@mhs.unj.ac.id>
2023-01-04 10:55:50 +0700

    update keranjang.php

--- keranjang.php
+++ keranjang.php
@@ -7,14 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="custom.css">
     <title>Warunk Ilkomp</title>
 </head>
 
 <body>
-    <?php include 'includes/navbar.php'?>
+    <?php include 'includes/navbar.php' ?>
     <div class="container" style="margin-top: 150px;">
         <div class="row">
             <div class="col-8">
@@ -31,12 +30,19 @@
                     <label class="form-check-label mt-1 ms-3" for="flexCheckDefault">
                         <div class="row">
                             <div class="col-3">
-                                <img src="assets/image/batre.png" class="cart-img-checkbox rounded col-3 float-start"
-                                    alt="">
+                                <img src="assets/image/batre.png" class="cart-img-checkbox rounded col-3 float-start" alt="">
                             </div>
-                            <div class="col overflow-hidden">
+                            <div class="col d-flex flex-column overflow-hidden">
                                 <div class="h6 fw-normal">Meja Gaming Meja Komputer GG Gemink FLOTH FL120 - Merah</div>
                                 <div class="h4 mt-2 fw-bold">Rp 500.000</div>
+                                <div class="h4 pt-5 align-self-end d-flex">
+                                    <a href="">
+                                        <i class="fa-solid fa-trash" style="font-size: 25px;"></i>
+                                    </a>
+                                    <div class="counter ms-4">
+                                        Counter
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </label>
@@ -47,8 +53,7 @@
                     <label class="form-check-label mt-1 ms-3" for="flexCheckDefault">
                         <div class="row">
                             <div class="col-3">
-                                <img src="assets/image/batre.png" class="cart-img-checkbox rounded col-3 float-start"
-                                    alt="">
+                                <img src="assets/image/batre.png" class="cart-img-checkbox rounded col-3 float-start" alt="">
                             </div>
                             <div class="col overflow-hidden">
                                 <div class="h6 fw-normal">Meja Gaming Meja Komputer GG Gemink FLOTH FL120 - Merah</div>
@@ -91,9 +96,7 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- Font Awesome Script -->
     <script src="https://kit.fontawesome.com/0958c69058.js" crossorigin="anonymous"></script>
